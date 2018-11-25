253752dad50414427a7504ec6cfe59555fdbdd9b
dangoldstein <dangoldstein3@gmail.com>
2018-11-25 10:22:43 +0200

    step-2: add bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { FiltersComponent } from './filters/filters.component';
-import { GridComponent } from './grid/grid.component';
-import { QuickActionComponent } from './quick-action/quick-action.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent,
-    FiltersComponent,
-    GridComponent,
-    QuickActionComponent
+    NavComponent
   ],
   imports: [
     BrowserModule
