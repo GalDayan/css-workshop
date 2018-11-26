9a6b7b52924b37c254526ef76a4d001d76799a55
Gal-PC\Gal <galdayan1895@gmail.com>
2018-11-26 09:20:53 +0200

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  dontClick() {
-    alert("מספיק ללחוץ על כפתורים!");
-    alert("כנס ל-VS Code ותתחיל לעבוד!");
-    window.location.assign("https://www.thatsfarming.com/uploads/news/1200x800_8-208a347d63693d952b7c8a3b7338ac58/3408-alpaca-1191300-1280.jpg");
-  }
+  title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FiltersComponent } from './filters/filters.component';
+import { GridComponent } from './grid/grid.component';
+import { QuickActionComponent } from './quick-action/quick-action.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FiltersComponent,
+    GridComponent,
+    QuickActionComponent
   ],
   imports: [
     BrowserModule
